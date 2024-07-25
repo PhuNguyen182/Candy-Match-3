217a2fe23bb226b94a73f1a69c6a456be30fe917
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-07-25 22:06:17 +0700

    Modify meta, update ruled random tile ids

--- Assets/Candy Match 3/Scripts/Level Design/Custom Tiles/Item Tiles/RuledRandomTile.cs
+++ Assets/Candy Match 3/Scripts/Level Design/Custom Tiles/Item Tiles/RuledRandomTile.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using CandyMatch3.Scripts.Common.Enums;
 using UnityEngine;
 
 namespace CandyMatch3.Scripts.LevelDesign.CustomTiles
@@ -8,8 +7,6 @@ namespace CandyMatch3.Scripts.LevelDesign.CustomTiles
     [CreateAssetMenu(fileName = "Ruled Random Tile", menuName = "Scriptable Objects/Level Design/Tiles/Ruled Random Tile")]
     public class RuledRandomTile : SingleItemTile
     {
-        [SerializeField] private CandyColor candyColor;
 
-        public CandyColor CandyColor => candyColor;
     }
 }
