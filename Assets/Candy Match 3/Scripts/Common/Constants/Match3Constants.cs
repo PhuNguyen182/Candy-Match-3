e2501fba2330a751c0ad11829445b521b40abd6e
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-08-29 15:43:14 +0700

    Update movement speed

--- Assets/Candy Match 3/Scripts/Common/Constants/Match3Constants.cs
+++ Assets/Candy Match 3/Scripts/Common/Constants/Match3Constants.cs
@@ -6,8 +6,8 @@ namespace CandyMatch3.Scripts.Common.Constants
         public const float ItemReleaseDelay = 0.25f;
         public const float ExplodeAmplitude = 1f;
 
-        public const float BaseItemMoveSpeed = 12f;
-        public const float FallenAccelaration = 1.8f;
+        public const float BaseItemMoveSpeed = 9f;
+        public const float FallenAccelaration = 1.618033988f;
         public const float TouchMoveTorerance = 0.25f; // 0.5f * 0.5f
     }
 }
