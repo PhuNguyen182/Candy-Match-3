114b455a52d30444d52881046139b2003d759768
PhuNguyen182 <52516080+PhuNguyen182@users.noreply.github.com>
2024-09-16 16:29:23 +0700

    Update starbar

--- Assets/Candy Match 3/Scripts/Gameplay/Game UI/Main Screen/StarStreak.cs
+++ Assets/Candy Match 3/Scripts/Gameplay/Game UI/Main Screen/StarStreak.cs
@@ -7,10 +7,17 @@ namespace CandyMatch3.Scripts.Gameplay.GameUI.MainScreen
     public class StarStreak : MonoBehaviour
     {
         [SerializeField] private GameObject star;
+        [SerializeField] private RectTransform rectTransform;
 
         public void SetStarActive(bool active)
         {
             star.SetActive(active);
+        }
+
+        public void SetPosition(float x)
+        {
+            float y = rectTransform.anchoredPosition.y;
+            rectTransform.anchoredPosition = new(x, y);
         }
     }
 }
